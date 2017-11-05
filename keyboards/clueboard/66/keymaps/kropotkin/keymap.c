a17811a8f9e57e40041dfde645828d3ef102b7f7
lukelliot <lucaselliotchin@gmail.com>
2017-11-05 11:33:13 -0500

    spacing

--- keyboards/clueboard/66/keymaps/kropotkin/keymap.c
+++ keyboards/clueboard/66/keymaps/kropotkin/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_FL] = KEYMAP(
   RGB_TOG,  KC_F1,  KC_F2,  KC_F3,  KC_F4, BL_DEC, BL_INC,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,        RGB_VAI, \
-  RGB_MOD,_______,RGB_SAI,_______,RESET  ,_______,_______,_______,_______,_______,KC_MPLY,KC_MRWD,KC_MFFD,KC_MUTE,                RGB_VAD, \
+  RGB_MOD,_______,RGB_SAI,_______,  RESET,_______,_______,_______,_______,_______,KC_MPLY,KC_MRWD,KC_MFFD,KC_MUTE,                RGB_VAD, \
   _______,RGB_HUD,RGB_SAD,RGB_HUI,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,                         \
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        KC_PGUP,         \
   _______,_______,_______,_______,        _______,_______,                        _______,_______,_______,_______,KC_HOME,KC_PGDN,KC_END),
